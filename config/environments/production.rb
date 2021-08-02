6465ee7855429bfab8947f6b41168acd396b88d9
cabrace <christopher.a.brace@gmail.com>
2021-08-02 01:25:14 -0400

    Renamed project with rename gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "belongs_to_production"
+  # config.active_job.queue_name_prefix = "rails002_belongs_to_adding_style_production"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
